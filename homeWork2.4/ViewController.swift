0071edb2be71a761a1fe98a24f17577257512730
Alexey Isaev <montilarard@gmail.com>
2022-11-12 17:08:54 +0300

    completed

--- homeWork2.4/ViewController.swift
+++ homeWork2.4/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redValue: UILabel!
@@ -23,21 +23,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 10
+        changeColor()
     }
-
-
-    @IBAction func redSliderAction() {
+    
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        changeColor()
         redValue.text = String(round(redSlider.value * 100) / 100)
     }
     
-    
-    @IBAction func greenSliderAction() {
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        changeColor()
         greenValue.text = String(round(greenSlider.value * 100) / 100)
+  
     }
     
-    
-    @IBAction func blueSliderAction() {
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+        changeColor()
         blueValue.text = String(round(blueSlider.value * 100) / 100)
     }
+    
+    private func changeColor () {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
-
