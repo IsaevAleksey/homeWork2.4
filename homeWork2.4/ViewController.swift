ca5d15eefaf79d870d753a324f5b724fe9f47d66
Alexey Isaev <montilarard@gmail.com>
2022-11-12 13:01:36 +0300

    Initial Commit

---
+++ homeWork2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork2.4
+//
+//  Created by Алексей Исаев on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
