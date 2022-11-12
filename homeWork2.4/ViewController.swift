8addd9d200b15edd2abed6814b1d3aa70d31421d
Alexey Isaev <montilarard@gmail.com>
2022-11-12 15:55:47 +0300

    logic

--- homeWork2.4/ViewController.swift
+++ homeWork2.4/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
     }
 
 
+    @IBAction func redSliderAction() {
+        redValue.text = String(round(redSlider.value * 100) / 100)
+    }
+    
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(round(greenSlider.value * 100) / 100)
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(round(blueSlider.value * 100) / 100)
+    }
 }
 
